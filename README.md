716a293f35eef54c8b1aed760a2deb87ae512ccc
Sujeeth <sujeeth.anil@gmail.com>
2020-09-22 22:20:00 +0530

    Added query parser

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ pip install -r pip_reqs.txt
 - Each module has to be included in main.py
 - main.py will be the main file used for full system and integration testing
 - Update README.md and/or requirements.txt with all libraries that need to be installed. This helps all teammates to run code.
+- Please add function description at the beginning of the function in triple quotes and one word for input parameter and return variable(example can be see in query_parser.py).
---
+++ query_parser.py
@@ -0,0 +1,25 @@
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
+
+query= "videos of forehand serve  in tennis"
+
+def get_Keywords(text):
+	'''
+	Description:This function returns an array of keywords.
+
+	input parameter => query sentence
+	output => list of keywords
+
+	'''
+	tokenized = word_tokenize(text)
+	lemmatizer = WordNetLemmatizer()
+	tokens = [lemmatizer.lemmatize(token) for token in tokenized] 
+	tags = pos_tag(tokenized)
+	nouns = [word for word, pos in tags if pos == 'NNP' or pos == 'NN' or pos == 'NNP' or pos == 'NNPS' or pos == 'VBG' or pos == 'VBD' or pos == 'VBP' or pos == 'VBP']
+	return nouns
+
+print(get_Keywords(query))
