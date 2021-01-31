f25a1096f8ac239d506759a85b9bf5284daf2626
Sujeeth Vankudari <sujeeth.anil@gmail.com>
2021-02-01 01:03:30 +0530

    Added Insert paser with Trigger Functions

--- Parser/parse.py
+++ Parser/parse.py
@@ -1,82 +1,129 @@
 from pyparsing import (
-    Word,
     delimitedList,
-    Optional,
     Group,
-    alphas,
-    alphanums,
     Forward,
-    oneOf,
-    quotedString,
-    infixNotation,
-    opAssoc,
-    restOfLine,
     CaselessKeyword,
     ParserElement,
-    OneOrMore,
-    originalTextFor,
     ZeroOrMore,
     QuotedString,
     pyparsing_common as ppc,
 )
 
-ParserElement.enablePackrat()
 
-# define SQL tokens
-selectStmt = Forward()
-SELECT, FROM, DURING, END, OF, BEGIN, BY, AT, IN, NEAR, ON, CAUSING, CAUSED = map(
-    CaselessKeyword, "select from during end of begin by at in near on causing caused".split()
-)
-DURING_END_OF = DURING + END + OF
-BEGIN_OF = BEGIN + OF
-CAUSED_BY = CAUSED + BY
+def defineGrammar():
 
-ident = QuotedString("'").setName("identifier")
-columnName = ident.setName("event name")
-columnName.addParseAction(ppc.downcaseTokens)
-columnNameList = Group(delimitedList(columnName))
+    """
+    This Function defines the grammar for Insert and Select statements
+    return:Parse Element Object
+    """
+    ParserElement.enablePackrat()
+
+    # define SQL tokens
+    selectStmt = Forward()
+    insertStmt = Forward()
+    INSERT, INTO, SELECT, FROM, DURING, END, OF, BEGIN, BY, AT, IN, NEAR, ON, CAUSING, CAUSED = map(
+        CaselessKeyword, "insert into select from during end of begin by at in near on causing caused".split()
+    )
+
+    DURING_END_OF = DURING + END + OF
+    BEGIN_OF = BEGIN + OF
+    CAUSED_BY = CAUSED + BY
+
+    ident = QuotedString("'").setName("identifier")
+    columnName = ident.setName("event name")
+    columnName.addParseAction(ppc.downcaseTokens)
+    columnNameList = Group(delimitedList(columnName))
+
+    tableName = ident.setName("database name")
+    tableName.addParseAction(ppc.downcaseTokens)
+    tableNameList = Group(delimitedList(tableName))
+
+    temporalName = ident.setName("temporal name")
+    temporalName.addParseAction(ppc.downcaseTokens)
+    temporalNameList = Group(delimitedList(temporalName))
+
+    spatialName = ident.setName("spatial name")
+    spatialName.addParseAction(ppc.downcaseTokens)
+    spatialNameList = Group(delimitedList(spatialName))
+
+    informationalName = ident.setName("informational name")
+    informationalName.addParseAction(ppc.downcaseTokens)
+    informationalNameList = Group(delimitedList(informationalName))
 
 
-tableName = ident.setName("database name")
-tableName.addParseAction(ppc.downcaseTokens)
-tableNameList = Group(delimitedList(tableName))
+    dbName = ident.setName("database name")
+    dbName.addParseAction(ppc.downcaseTokens)
 
-temporalName = ident.setName("temporal name")
-temporalName.addParseAction(ppc.downcaseTokens)
-temporalNameList = Group(delimitedList(temporalName))
+    csvName = ident.setName("csv name")
 
-spatialName = ident.setName("spatial name")
-spatialName.addParseAction(ppc.downcaseTokens)
-spatialNameList = Group(delimitedList(spatialName))
+    causalityName = ident.setName("causality name")
+    causalityName.addParseAction(ppc.downcaseTokens)
+    causalityNameList = Group(delimitedList(causalityName))
 
-informationalName = ident.setName("informational name")
-informationalName.addParseAction(ppc.downcaseTokens)
-informationalNameList = Group(delimitedList(informationalName))
+    propertyList = (
+                    ZeroOrMore((BEGIN_OF | DURING_END_OF) + temporalNameList("temporal"))
+                    & ZeroOrMore(BY + informationalNameList("informational"))
+                    & ZeroOrMore((AT | IN | NEAR | ON) + spatialNameList("spatial"))
+                    & ZeroOrMore((CAUSING | CAUSED_BY) + causalityNameList("causality"))
+                    )
 
-causalityName = ident.setName("causality name")
-causalityName.addParseAction(ppc.downcaseTokens)
-causalityNameList = Group(delimitedList(causalityName))
+    # define the grammar
 
-propertyList = (
-                ZeroOrMore((BEGIN_OF | DURING_END_OF) + temporalNameList("temporal"))
-                & ZeroOrMore(BY + informationalNameList("informational"))
-                & ZeroOrMore((AT | IN | NEAR | ON) + spatialNameList("spatial"))
-                & ZeroOrMore((CAUSING | CAUSED_BY) + causalityNameList("causality"))
-                )
+    selectStmt <<= (
+        SELECT + columnNameList("event")
+        + propertyList
+        + FROM + tableNameList("database")
+    )
+    insertStmt <<= (
+            INSERT + csvName("csv_filename")
+            + INTO + dbName("database")
+    )
 
-# define the grammar
+    simpleSQL = selectStmt.addParseAction(select_fn) | insertStmt.addParseAction(insert_fn)
 
-selectStmt <<= (
-    SELECT + columnNameList("event")
-    + propertyList
-    + FROM + tableNameList("database")
-)
+    return simpleSQL
 
-simpleSQL = selectStmt
+
+def insert_fn(output):
+    """
+    This Function is triggered whenever a Insert Query is Matched
+    :return:
+    """
+    #here comes insert operation
+
+    #below is the example access of data
+    print (output.csv_filename)
+    print (output.database)
+    return
+
+
+def select_fn(output):
+    """
+    This Function is triggered whenever a Select Query is Matched
+    :return:
+    """
+
+    #here comes select operation
+
+    #below is the example access of data
+    print (output.spatial)
+    print (output.informational)
+    print (output.causality)
+    print (output.temporal)
+    return
 
 
 if __name__ == "__main__":
-    simpleSQL.runTests(
+
+    grammar = defineGrammar()
+    result = grammar.parseString(
+        """\
+        INSERT 'Data.csv'\
+        INTO 'IndvsAus'
+        """
+
+    )
+    result=grammar.parseString(
         """\
         SELECT 'hitting six' \
         at 'adelaide' \
