db3a1ba80d867a598270f9eb586b8ecf7e473949
navneetraju66 <navneetraju66@gmail.com>
2021-03-21 19:26:42 +0530

    Handler exceptions and logging

--- Handler.py
+++ Handler.py
@@ -3,8 +3,11 @@ import Neo4jConnection
 from DataParsing import dataparsing
 import sys
 import Constants
-import pandas as pd
+from Exceptions import *
 import json
+import logging
+import properties
+logging.basicConfig(format='%(asctime)s %(message)s',level=properties.LOG_LEVEL)
 
 
 parserObj = parse.Parser()
@@ -16,13 +19,15 @@ class Handler:
         self.queryProcessor = dataparsing.DataParser()
 
     def query(self,requestQuery:str,fuzzy:bool):
+        logging.info("Recieved query request, starting handling...")
         if requestQuery == None or len(requestQuery) == 0:
             return json.dumps({"code":400,"message":"Invalid request query"})
         parsedDictionary = None 
         try:
             parsedDictionary = self.parserObj.parseQuery(requestQuery)[0]
         except Exception as e:
-            return json.dumps({"code":400,"message":"Invalid request query: "+ str(e)})
+            logging.error("Handling parsing exception ",exc_info=True)
+            return json.dumps({"code":400,"message":"Query syntax error: "+ str(e)})
 
         if parsedDictionary['type'] == Constants.INSERT:
             #To-do @Durga to add insert object call and handle exceptions
@@ -30,13 +35,16 @@ class Handler:
         elif parsedDictionary['type'] == Constants.SELECT:
             result = None
             if fuzzy:
+                logging.info("Starting fuzzy query")
                 result = self.queryProcessor.fuzzyQuery(parsedDictionary)
             else:
+                logging.info("Starting simple query")
                 result = self.queryProcessor.query(parsedDictionary)
             return result
 
 
-
+'''
+For reference only
 if __name__ == "__main__":
     handler = Handler()
     while True:
@@ -50,3 +58,4 @@ if __name__ == "__main__":
                 user_writing.append(line) 
         user_writing = '\n'.join(user_writing) 
         print(handler.query(user_writing,True))
+'''
