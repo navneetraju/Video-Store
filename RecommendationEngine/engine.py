a301b79082aedbc7ee6876d517c064e903e4e960
sujeethav <sujeeth.anil@gmail.com>
2021-04-13 16:15:25 +0530

    Added LRU cache

--- RecommendationEngine/engine.py
+++ RecommendationEngine/engine.py
@@ -3,6 +3,7 @@ from Neo4jConnection import Neo4jConnection
 import Constants
 import properties
 import logging
+from cache import timed_lru_cache
 logging.basicConfig(format='%(asctime)s %(message)s',level=properties.LOG_LEVEL)
 
 class QueryEngine:
@@ -10,6 +11,7 @@ class QueryEngine:
     def __init__(self):
         self.conn = Neo4jConnection(uri=properties.NEO4J_SERVER_URL, user=properties.NEO4J_SERVER_USERNAME, pwd=properties.NEO4J_SERVER_PASSWORD)
 
+    @timed_lru_cache(300)
     def get_recommendation(self,videoId,start_frame,end_frame,dbName):
 
         recommendation_query = Constants.NEO4J_RECOMMENDATION_QUERY(videoId,start_frame,end_frame)
