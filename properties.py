03d3c5f3ddce4491a6df2bdc46b896dffa401e7d
navneetraju66 <navneetraju66@gmail.com>
2021-04-16 21:14:19 +0530

    CS urls

--- properties.py
+++ properties.py
@@ -5,3 +5,6 @@ NEO4J_SERVER_USERNAME = "neo4j"
 NEO4J_SERVER_PASSWORD = "mySecretPassword"
 GOOGLE_API_KEY = "AIzaSyCChUK1mpjVQE7eEc2GSeobACpSlNgqJcA"
 LOG_LEVEL = INFO
+
+CONFIG_SERVICE_DB_URL = "http://10.10.1.146:8080/api/config/data?database={}"
+CONFIG_SERVICE_MAPPING_URL = "http://10.10.1.146:8080/api/config/mapping?configName={}"
