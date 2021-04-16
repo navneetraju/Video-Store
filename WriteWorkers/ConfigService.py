39e54d52d1496b9cb792dd06e31ccb6797541d2b
navneetraju66 <navneetraju66@gmail.com>
2021-04-16 21:13:52 +0530

    Created writeworker directory and ConfigService class

---
+++ WriteWorkers/ConfigService.py
@@ -0,0 +1,49 @@
+import os, sys
+import functools
+import requests
+import logging
+import json
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import properties
+
+from cache import timed_lru_cache
+
+class ConfigService:
+
+	@timed_lru_cache(600)
+	def getDatabaseConfig(self,database):
+		config = requests.get(properties.CONFIG_SERVICE_DB_URL.format(database))
+		res = dict()
+		if config.status_code == 500:
+			logging.error("ConfigService error while fetching database config")
+			res['status'] = 500
+			res['config'] = None 
+			return res
+		elif config.status_code == 400:
+			logging.error("ConfigService could not find the config for the database:{}".format(database))
+			res['status'] = 400
+			res['config'] = None 
+			return res
+		res['status'] = 200
+		res['config'] = config.json()
+		return res
+
+	@timed_lru_cache(600)
+	def getCategoryMapping(self,mappingName):
+		mapppingResponse = requests.get(properties.CONFIG_SERVICE_MAPPING_URL.format(mappingName))
+		res = dict()
+		if mapppingResponse.status_code == 500:
+			logging.error("ConfigService error while fetching database config")
+			res['status'] = 500
+			res['mapping'] = None 
+			return res
+		elif mapppingResponse.status_code == 400:
+			logging.error("ConfigService could not find the mapping :{}".format(mappingName))
+			res['status'] = 400
+			res['mapping'] = None 
+			return res
+		res['status'] = 200
+		res['mapping'] = mapppingResponse.json()
+		return res
